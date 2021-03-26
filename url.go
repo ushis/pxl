f84bf693acb7ee4faef78356332b8921a63bd8fb
ushi <ushi@honkgong.info>
2021-03-26 19:58:59 +0100

    introduce EncodeURL

--- url.go
+++ url.go
@@ -10,12 +10,12 @@ import (
 )
 
 func DecodeURL(url *url.URL) (Pxl, *EncodingOptions, error) {
-	pxl, err := decodePath(url.Path)
+	pxl, err := decodeURLPath(url.Path)
 
 	if err != nil {
 		return nil, nil, err
 	}
-	opts, err := decodeQuery(url.Query())
+	opts, err := decodeURLQuery(url.Query())
 
 	if err != nil {
 		return nil, nil, err
@@ -23,7 +23,7 @@ func DecodeURL(url *url.URL) (Pxl, *EncodingOptions, error) {
 	return pxl, opts, err
 }
 
-func decodePath(pth string) (Pxl, error) {
+func decodeURLPath(pth string) (Pxl, error) {
 	pth = pth[:len(pth)-len(path.Ext(pth))]
 
 	if pth == "" || pth[0] != '/' {
@@ -43,7 +43,7 @@ func decodePath(pth string) (Pxl, error) {
 	return pxl, nil
 }
 
-func decodeQuery(params url.Values) (*EncodingOptions, error) {
+func decodeURLQuery(params url.Values) (*EncodingOptions, error) {
 	opts := &EncodingOptions{
 		Fg:    defaultEncodingOptions.Fg,
 		Bg:    defaultEncodingOptions.Bg,
@@ -53,7 +53,7 @@ func decodeQuery(params url.Values) (*EncodingOptions, error) {
 		Scale: defaultEncodingOptions.Scale,
 	}
 	if str := params.Get("fg"); str != "" {
-		clr, err := decodeColor(str)
+		clr, err := decodeURLColor(str)
 
 		if err != nil {
 			return opts, err
@@ -61,7 +61,7 @@ func decodeQuery(params url.Values) (*EncodingOptions, error) {
 		opts.Fg = clr
 	}
 	if str := params.Get("bg"); str != "" {
-		clr, err := decodeColor(str)
+		clr, err := decodeURLColor(str)
 
 		if err != nil {
 			return opts, err
@@ -69,16 +69,20 @@ func decodeQuery(params url.Values) (*EncodingOptions, error) {
 		opts.Bg = clr
 	}
 	if str := params.Get("fc"); str != "" {
-		if len(str) != 1 {
+		chars := []rune(str)
+
+		if len(chars) != 1 {
 			return opts, errors.New("fc too long")
 		}
-		opts.Fc = []rune(str)[0]
+		opts.Fc = chars[0]
 	}
 	if str := params.Get("bc"); str != "" {
-		if len(str) != 1 {
+		chars := []rune(str)
+
+		if len(chars) != 1 {
 			return opts, errors.New("bc too long")
 		}
-		opts.Bc = []rune(str)[0]
+		opts.Bc = chars[0]
 	}
 	if str := params.Get("fps"); str != "" {
 		fps, err := strconv.ParseUint(str, 10, 8)
@@ -102,7 +106,7 @@ func decodeQuery(params url.Values) (*EncodingOptions, error) {
 	return opts, nil
 }
 
-func decodeColor(s string) (color.NRGBA, error) {
+func decodeURLColor(s string) (color.NRGBA, error) {
 	val, err := strconv.ParseUint(s, 10, 32)
 
 	clr := color.NRGBA{
@@ -112,4 +116,41 @@ func decodeColor(s string) (color.NRGBA, error) {
 		A: uint8((val >> 0) & 0xff),
 	}
 	return clr, err
+}
+
+func EncodeURL(pxl Pxl, opts *EncodingOptions) *url.URL {
+	return &url.URL{
+		Path:     encodeURLPath(pxl),
+		RawQuery: encodeURLQuery(opts).Encode(),
+	}
+}
+
+func encodeURLPath(pxl Pxl) string {
+	b := &strings.Builder{}
+
+	for _, row := range pxl {
+		b.WriteByte('/')
+		b.WriteString(strconv.FormatUint(row, 10))
+	}
+	return b.String()
+}
+
+func encodeURLQuery(opts *EncodingOptions) url.Values {
+	return url.Values{
+		"bg":    {encodeURLColor(opts.Bg)},
+		"fg":    {encodeURLColor(opts.Fg)},
+		"bc":    {string(opts.Bc)},
+		"fc":    {string(opts.Fc)},
+		"fps":   {strconv.Itoa(opts.Fps)},
+		"scale": {strconv.Itoa(opts.Scale)},
+	}
+}
+
+func encodeURLColor(clr color.NRGBA) string {
+	var num uint64
+	num |= uint64(clr.R) << 24
+	num |= uint64(clr.G) << 16
+	num |= uint64(clr.B) << 8
+	num |= uint64(clr.A) << 0
+	return strconv.FormatUint(num, 10)
 }

2b4a14cf9e793f5d9cf7eda00cae93c8912c3f50
ushi <ushi@honkgong.info>
2021-03-27 18:44:04 +0100

    cleanup

--- gif.go
+++ gif.go
@@ -12,7 +12,7 @@ func EncodeGif(w io.Writer, pxl Pxl, opts *EncodingOptions) error {
 
 	if opts.Fps > 0 {
 		g.Image = []*image.Paletted{encodeGif(pxl, opts, false), encodeGif(pxl, opts, true)}
-		g.Delay = []int{100.0 / opts.Fps, 100.0 / opts.Fps}
+		g.Delay = []int{100 / opts.Fps, 100 / opts.Fps}
 	} else {
 		g.Image = []*image.Paletted{encodeGif(pxl, opts, false)}
 		g.Delay = []int{0}
@@ -24,18 +24,17 @@ func encodeGif(pxl Pxl, opts *EncodingOptions, invert bool) *image.Paletted {
 	rct := image.Rect(0, 0, pxl.Cols()*opts.Scale, pxl.Rows()*opts.Scale)
 	plt := color.Palette{opts.Bg, opts.Fg}
 	img := image.NewPaletted(rct, plt)
+	bg, fg := opts.Bg, opts.Fg
 
+	if invert {
+		bg, fg = opts.Fg, opts.Bg
+	}
 	for row := 0; row < pxl.Rows(); row++ {
 		for col := 0; col < pxl.Cols(); col++ {
-			fg := pxl.Get(col, row)
+			clr := bg
 
-			if invert {
-				fg = !fg
-			}
-			clr := opts.Bg
-
-			if fg {
-				clr = opts.Fg
+			if pxl.Get(col, row) {
+				clr = fg
 			}
 			for x := col * opts.Scale; x < (col+1)*opts.Scale; x++ {
 				for y := row * opts.Scale; y < (row+1)*opts.Scale; y++ {
